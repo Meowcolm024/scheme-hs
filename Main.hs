b241492efe5934d3c1c71396d3df1663164cbd57
Meowcolm024 <luoyicong1026@live.com>
2020-07-14 07:23:38 +0800

    more

--- Main.hs
+++ Main.hs
@@ -4,6 +4,9 @@ module Main where
 import           Text.ParserCombinators.Parsec
                                          hiding ( spaces )
 import           Control.Monad.Except
+import           System.IO                      ( hFlush
+                                                , stdout
+                                                )
 
 data LispVal = Atom String
              | List [LispVal]
@@ -11,6 +14,7 @@ data LispVal = Atom String
              | Number Integer
              | Str String
              | Boolean Bool
+             deriving Eq
 
 data LispError = NumArgs Integer [LispVal]
                | TypeMismatch String LispVal
@@ -42,22 +46,38 @@ trapError a = catchError a (return . show)
 extractValue :: ThrowsError a -> a
 extractValue (Right x) = x
 
+{-
+IO part
+-}
+
 main :: IO ()
-main =
-    putStr "> "
-        >>  getLine
-        >>= ( putStrLn
-            . extractValue
-            . trapError
-            . fmap show
-            . (readExpr >=> eval)
-            )
-        >>  main
+main = flushStr "Scheme\n" >> runRepl
 
 readExpr :: String -> ThrowsError LispVal
 readExpr x = case regularParse parseExpr x of
     Left  err -> throwError $ Parser err
     Right val -> return val
+
+flushStr :: String -> IO ()
+flushStr s = putStr s >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString =
+    return . extractValue . trapError . fmap show . (readExpr >=> eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint = (putStrLn =<<) . evalString
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ cond prompt act = prompt
+    >>= \r -> if cond r then return () else act r >> until_ cond prompt act
+
+runRepl :: IO ()
+runRepl =
+    until_ (\x -> x == "quit" || x == "#q") (readPrompt "> ") evalAndPrint
 
 {-
 The parser part
@@ -112,15 +132,15 @@ The evaluation part
 -}
 
 eval :: LispVal -> ThrowsError LispVal
-eval val@(Str _) = return val
-eval val@(Number _) = return val
-eval val@(Boolean _) = return val
-eval (List [Atom "quote", val]) = return val
-eval (List [Atom "if", cond, conseq, alt]) = eval cond >>= \case
+eval val@(Str     _                             ) = return val
+eval val@(Number  _                             ) = return val
+eval val@(Boolean _                             ) = return val
+eval (    List    [Atom "quote", val]           ) = return val
+eval (    List    [Atom "if", cond, conseq, alt]) = eval cond >>= \case
     Boolean False -> eval alt
     _             -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
-eval _ = throwError $ Default "cannot evaluate"
+eval _                         = throwError $ Default "cannot evaluate"
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 apply func args =
@@ -131,10 +151,24 @@ primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives =
     [ ("+"        , numBinop (+))
     , ("-"        , numBinop (-))
+    , ("*"        , numBinop (*))
     , ("/"        , numBinop div)
     , ("mod"      , numBinop mod)
     , ("quotient" , numBinop quot)
     , ("remainder", numBinop rem)
+    , ("="        , numBoolBinop (==))
+    , (">"        , numBoolBinop (>))
+    , ("<"        , numBoolBinop (<))
+    , ("/="       , numBoolBinop (/=))
+    , (">="       , numBoolBinop (>=))
+    , ("<="       , numBoolBinop (<=))
+    , ("&&"       , boolBoolBinop (&&))
+    , ("||"       , boolBoolBinop (||))
+    , ("string=?" , strBoolBinop (==))
+    , ("car"      , car)
+    , ("cdr"      , cdr)
+    , ("cons"     , cons)
+    , ("eq?"      , eqv)
     ]
 
 numBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
@@ -165,3 +199,26 @@ boolBinop unpacker op args = if length args /= 2
 numBoolBinop = boolBinop unpackNum
 strBoolBinop = boolBinop unpackStr
 boolBoolBinop = boolBinop unpackBool
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x : _)        ] = return x
+car [DottedList (x : _) _] = return x
+car [badArg              ] = throwError $ TypeMismatch "pair" badArg
+car badArgList             = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (_ : xs)        ] = return $ List xs
+cdr [DottedList [_     ] x] = return x
+cdr [DottedList (_ : xs) x] = return $ DottedList xs x
+cdr [badArg               ] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList              = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x, List xs        ] = return $ List (x : xs)
+cons [x, DottedList xs y] = return $ DottedList (x : xs) y
+cons [a, b              ] = return $ DottedList [a] b
+cons badArgList           = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [x, y]     = return $ Boolean (x == y)
+eqv badArgList = throwError $ NumArgs 2 badArgList
